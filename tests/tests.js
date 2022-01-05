3f174ad9d530228950d1c6482c6518a4f3422765
saneef <hello@saneef.com>
2022-01-05 15:30:59 +0530

    fix: remove code to process 'contents'
    This was necessary in older versions of Eleventy.
    From Eleventy 1.0.0-beta.10 onwards this is not necessary.

--- tests/tests.js
+++ tests/tests.js
@@ -12,24 +12,10 @@ const outputBase = path.join("tests/output/");
 test.before("Cleanup output", async () => rimraf(outputBase));
 test.after.always("Cleanup output", async () => rimraf(outputBase));
 
-/**
- * Removes newlines.
- *
- * @param      {string}  str     The string
- * @return     {string}  Result
- */
-function removeNewlines(str) {
-  return str.replace(/[\n\r]+/g, " ");
-}
-
 test("Render AsciiDoc", async (t) => {
   const processor = eleventyAsciidoc();
-  const compile = processor.compile();
-  const result = compile({
-    page: {
-      inputPath: path.join(sourcePath, "hello.adoc"),
-    },
-  });
+  const compile = processor.compile(null, path.join(sourcePath, "hello.adoc"));
+  const result = compile();
   const output = `<div class="paragraph">
 <p>This text is written in AsciiDoc format.</p>
 </div>`;
@@ -41,42 +27,16 @@ test("Render AsciiDoc with converter options", async (t) => {
   const processor = eleventyAsciidoc({
     attributes: { showtitle: false },
   });
-  const compile = processor.compile();
-  const result = compile({
-    page: {
-      inputPath: path.join(sourcePath, "with-asciidoc-attributes.adoc"),
-    },
-  });
+  const compile = processor.compile(
+    null,
+    path.join(sourcePath, "with-asciidoc-attributes.adoc")
+  );
+  const result = compile();
   const output = `<div class="paragraph">
 <p>This text is written in AsciiDoc format.</p>
 </div>`;
 
   t.is(result, output);
-});
-
-test("Passback str if is 'string'", async (t) => {
-  const input = "permalink";
-  const processor = eleventyAsciidoc();
-  const compile = processor.compile("permalink");
-  const result = compile({
-    page: {
-      inputPath: path.join(sourcePath, "hello.adoc"),
-    },
-  });
-
-  t.is(result, input);
-});
-
-test("Apply str if it is 'function'", async (t) => {
-  const input = `<div class="paragraph">
-<p>This text is written in AsciiDoc format.</p>
-</div>`;
-
-  const processor = eleventyAsciidoc();
-  const compile = processor.compile(removeNewlines);
-  const result = compile(input);
-
-  t.is(result, removeNewlines(input));
 });
 
 test("Get data from front matter", async (t) => {
